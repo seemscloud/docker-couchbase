e758ea6084ed409abff0da7a88e5d55dc4ef9389
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-09-14 23:37:59 +0200

    no message

--- couchbase/entrypoint.sh
+++ couchbase/entrypoint.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-/bin/bash ./bin/install/reloc.sh `pwd`
+if [[ "${NODE_TYPE}" == "init" ]]; then
+  ${APPDIR}/${VERSION}/bin/couchbase-server --start
+  sleep 5
 
-pwd
+  couchbase-cli cluster-init \
+    --cluster $(hostname -i) --cluster-name SeemsCloud \
+    --cluster-username ${USERNAME} --cluster-password ${PASSWORD} \
+    --cluster-ramsize 4096 --cluster-index-ramsize 256 --cluster-fts-ramsize 256 \
+    --cluster-eventing-ramsize 256 --cluster-analytics-ramsize 1024
 
-couchbase-server
+  ${APPDIR}/${VERSION}/bin/couchbase-server --stop
+  sleep 5
+elif [[ ${NODE_TYPE} == "join" ]]; then
+  ${APPDIR}/${VERSION}/bin/couchbase-server --start
+  sleep 5
+
+  curl http://couchbase-init:8091/controller/addNode \
+    -v -X POST \
+    -u "${USERNAME}:${PASSWORD}" \
+    -d "hostname=http://$(hostname -i)" \
+    -d "user=${USERNAME}" \
+    -d "password=${PASSWORD}" \
+    -d 'services=kv,n1ql,index'
+
+  ${APPDIR}/${VERSION}/bin/couchbase-server --stop
+  sleep 5
+fi
+
+${APPDIR}/${VERSION}/bin/couchbase-server -- -kernel global_enable_tracing false -noinput
