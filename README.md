37661eb9adf9e9fc4e8e33127fb09f8e559c7eb6
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2021-10-05 00:39:29 +0200

    no message

--- README.md
+++
@@ -1,1 +0,0 @@
-# docker-couchbase
--- couchbase/entrypoint.sh
+++ couchbase/entrypoint.sh
@@ -10,6 +10,19 @@ if [[ "${NODE_TYPE}" == "init" ]]; then
     --cluster-ramsize 4096 --cluster-index-ramsize 256 --cluster-fts-ramsize 256 \
     --cluster-eventing-ramsize 256 --cluster-analytics-ramsize 1024
 
+  couchbase-cli setting-ldap \
+    --cluster localhost --username admin --password admin1 \
+    --authentication-enabled 1 --authorization-enabled 1 \
+    --hosts ldap --port 389 --encryption none \
+    --bind-dn "uid=auth,ou=users,dc=seems,dc=cloud" --bind-password "auth" \
+    --user-dn-query "ou=users,dc=seems,dc=cloud??one?(uid=%u)" \
+    --group-query "ou=groups,dc=seems,dc=cloud??one?(&(objectclass=groupOfUniqueNames)(uniquemember=uid=%u,ou=users,dc=seems,dc=cloud))"
+
+  couchbase-cli user-manage \
+    --cluster localhost --username admin --password admin1 \
+    --set-group --group-name admins --roles admin \
+    --ldap-ref "cn=couchbase_ui,ou=groups,dc=seems,dc=cloud"
+
   ${APPDIR}/${VERSION}/bin/couchbase-server --stop
   sleep 5
 elif [[ ${NODE_TYPE} == "join" ]]; then
